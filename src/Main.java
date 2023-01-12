638d6492d2c54231a2c07bd909291606c954db2e
Daniglez03 <gonzalezlorenzodani7@gmail.com>
2023-01-12 10:10:24 +0000

    (Main.java): Commit experimento 2

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,10 @@ public class Main {
 
         Huerto huerto = new Huerto(10);
 
-        Prodcutor p = new Prodcutor("Daniel", 10, 4, huerto);
+        Prodcutor p = new Prodcutor("Daniel", 5, 1, huerto);
+        Consumidor d = new Consumidor("Adrian", 5, 5, huerto);
 
-        Consumidor c = new Consumidor("Goyo", 5, 5, huerto);
-        Consumidor d = new Consumidor("Adrian", 5, 2, huerto);
         p.start();
-        c.start();
         d.start();
     }
 }
